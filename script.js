cc516f8482112ab401f346b09aec56bb38917a72
Alex Contreras <alex.code29@gmail.com>
2022-05-10 20:35:23 -0500

    display the current date and added the hours time block

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const hours = 9
+const workHours = [9, 10, 11, 12, 1, 2, 3, 4,5];
 
-for (let i=0;i<=hours;i++){
-    const li = $('<div>')
-        .addClass('')
-    const timeBlock = $('.container')
-    li.val(i)
-    timeBlock.append(li)
-    
+function timeOfDay() {
+    $('#currentDay').text(moment().format('dddd , MMM Do'))
 }
+
+function ap(timeArr) {
+    if (timeArr > 6 && timeArr < 12) {
+        return 'am'
+    }else {
+        return 'pm'
+    }
+}
+function displayBlocks() {
+    for (let i = 0; i < workHours.length; i++) {
+        const div = $('<div>').addClass('jumbotron row')
+
+        let timeDiv = $('<p>')
+            .addClass(' hour col-1')
+
+        let textarea = $('<textarea>').addClass('col-10')
+
+        let saveBtn = $('<button>').addClass('saveBtn col-1')
+        let timeBlock = $('.container')
+        timeDiv.text(`${workHours[i]} ${ap(workHours[i])}`)
+        div.append(timeDiv, textarea, saveBtn)
+        timeBlock.append(div)
+    }
+};
+
+
+displayBlocks();
+timeOfDay();
