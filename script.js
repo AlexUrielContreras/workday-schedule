9d0801a37ba83f1d05e7d62659a29884eef72293
Alex Contreras <alex.code29@gmail.com>
2022-05-10 19:41:46 -0500

    cloned starter code

---
+++ script.js
@@ -0,0 +1,10 @@
+const hours = 9
+
+for (let i=0;i<=hours;i++){
+    const li = $('<div>')
+        .addClass('')
+    const timeBlock = $('.container')
+    li.val(i)
+    timeBlock.append(li)
+    
+}
