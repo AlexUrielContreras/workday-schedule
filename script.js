d4049d1062d5dd4abd798a621595e1f16646b8d5
Alex Contreras <alex.code29@gmail.com>
2022-05-12 16:59:11 -0500

    grab the values from localstorage

--- script.js
+++ script.js
@@ -16,23 +16,21 @@ function displayBlocks() {
             .addClass(`hour col-1`)
 
         let textarea = $('<textarea>')
-            .addClass(`col-10 tasks ${blockColor(i+9)}`)
-            .attr('id', i + 9)
+            .addClass(`col-10 tasks ${blockColor(i+9)} hour${i}`)
 
-        let saveBtn = $('<button>').addClass('saveBtn col-1')
+        let saveBtn = $(`<button type="submit" id="${i}">`).addClass('saveBtn col-1')
         let timeBlock = $('.container')
         timeP.text(`${workHours[i]} ${timeOfDay(workHours[i])}`)
         div.append(timeP, textarea, saveBtn)
         timeBlock.append(div)
     }
+    saveTask()
 };
 
 
 
 function blockColor(data){
     let time = moment().format('H').split(':')[0]
-    console.log(time)
-    console.log(data)
     if (time < data){
         return 'future'
     }else if (time == data){
@@ -42,7 +40,25 @@ function blockColor(data){
     }
 }
 
+function saveTask() {
+    $('button').on('click', function(){
+        const id = $(this).attr('id')
+        const lol = `hour${id}`
+        const text = $(`.hour${id}`).val()
+        localStorage.setItem(lol, text)
+    })
+    $('.hour0').text(localStorage.getItem('hour0'));
+    $('.hour1').text(localStorage.getItem('hour1'));
+    $('.hour2').text(localStorage.getItem('hour2'));
+    $('.hour3').text(localStorage.getItem('hour3'));
+    $('.hour4').text(localStorage.getItem('hour4'));
+    $('.hour5').text(localStorage.getItem('hour5'));
+    $('.hour6').text(localStorage.getItem('hour6'));
+    $('.hour7').text(localStorage.getItem('hour7'));
+    $('.hour8').text(localStorage.getItem('hour8'));
+    
+}
+
+
 
 displayBlocks();
-
-
